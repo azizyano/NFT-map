dbe79e8ff590fbdc5a8a3e3f90409c00c3b50089
Aziz Yano <32180132+azizyano@users.noreply.github.com>
2021-04-08 21:53:44 +0100

    update the code

--- client/App.js
+++ client/App.js
@@ -14,7 +14,6 @@ import { toTxResult } from "@celo/connect"
 import * as Linking from 'expo-linking'
 import CeloBet from './contracts/CeloBet.json'
 
-
 YellowBox.ignoreWarnings(['Warning: The provided value \'moz', 'Warning: The provided value \'ms-stream'])
 
 export default class App extends React.Component {
@@ -28,165 +27,107 @@ export default class App extends React.Component {
   }
   constructor(props) {
     super(props);
-    this.state = {text: ''};
+    this.state = {amount: '0'};
   }
   // This function is called when the page successfully renders
   componentDidMount = async () => {
-    
-    // Check the Celo network ID
     const networkId = await web3.eth.net.getId();
-    
-    // Get the deployed HelloWorld contract info for the appropriate network ID
     const deployedNetwork = CeloBet.networks[networkId];
-
-    // Create a new contract instance with the HelloWorld contract info
     const instance = new web3.eth.Contract(
       CeloBet.abi,
       deployedNetwork && deployedNetwork.address
     );
-
-    // Save the contract instance
     this.setState({ CeloBet: instance })
   }
 
   login = async () => {
-    
-    // A string you can pass to DAppKit, that you can use to listen to the response for that request
     const requestId = 'login'
-    
-    // A string that will be displayed to the user, indicating the DApp requesting access/signature
     const dappName = 'Celo Bet'
-    
-    // The deeplink that the Celo Wallet will use to redirect the user back to the DApp with the appropriate payload.
     const callback = Linking.makeUrl('/my/path')
-  
-    // Ask the Celo Alfajores Wallet for user info
     requestAccountAddress({
       requestId,
       dappName,
       callback,
     })
-  
-    // Wait for the Celo Wallet response
     const dappkitResponse = await waitForAccountAuth(requestId)
-
-    // Set the default account to the account returned from the wallet
     kit.defaultAccount = dappkitResponse.address
     let balance = await kit.web3.eth.getBalance(dappkitResponse.address)
-    balance= balance/10**18
-    // Get the stabel token contract
+    balance = balance/10**18
     const stableToken = await kit.contracts.getStableToken()
-
-    // Get the user account balance (cUSD)
     const cUSDBalanceBig = await stableToken.balanceOf(kit.defaultAccount)
-    
-    // Convert from a big number to a string
     let cUSDBalance = cUSDBalanceBig.toString()/10**18
-    // Update state
-    this.setState({ cUSDBalance, 
-                    celoBalance: balance,
-                    isLoadingBalance: false,
-                    address: dappkitResponse.address })
+    this.setState({ cUSDBalance, celoBalance: balance, isLoadingBalance: false,
+    address: dappkitResponse.address })
   }
 
-  async makeBet(bet, amount) {
+  async makeBet(bet) {
     const requestId = 'spinup'
     const dappName = 'Celo Bet'
     const callback = Linking.makeUrl('/my/path')
     var randomSeed = Math.floor(Math.random() * Math.floor(1e9))
     const txObject = await this.state.CeloBet.methods.game(bet, randomSeed)
     await kit.sendTransactionObject(txObject, {from: this.state.address})
-    requestTxSig(
-      kit,
-      [
+    requestTxSig(kit,[
         {
           from: this.state.address,
           to: this.state.CeloBet.options.address,
           tx: txObject,
-          value: amount,
+          value: this.state.amount * 10**18,
           feeCurrency: FeeCurrency.cUSD
-        }
-      ],
+        }],
       { requestId, dappName, callback }
     )
     const dappkitResponse = await waitForSignedTxs(requestId)
     const tx = dappkitResponse.rawTxs[0]
     let result = await toTxResult(kit.web3.eth.sendSignedTransaction(tx)).waitReceipt()
-    console.log(`Hello World contract update transaction receipt: `, result)
-    this.state.CeloBet.events.Result({}, async (error, event) => {
-      const verdict = event.returnValues.winAmount
-      if(verdict === '0') {
-        console.log('lose :(')
-      } else {
-        console.log('WIN!')
-      }
-    })
+    console.log(`transaction receipt: `, result)
+    let balance = await kit.web3.eth.getBalance(this.state.address)
+    balance = balance/10**18
+    const stableToken = await kit.contracts.getStableToken()
+    const cUSDBalanceBig = await stableToken.balanceOf(kit.defaultAccount)
+    let cUSDBalance = cUSDBalanceBig.toString()/10**18
+    this.setState({ cUSDBalance, celoBalance: balance, isLoadingBalance: false })
   }
-  spindown = async () => {
-    const requestId = 'qpinup'
-    const dappName = 'Celo Bet'
-    const callback = Linking.makeUrl('/my/path')
-    var randomSeed = Math.floor(Math.random() * Math.floor(1e9))
-    const txObject = await this.state.CeloBet.methods.game(1, randomSeed)
-    await kit.sendTransactionObject(txObject, {from: this.state.address})
-    requestTxSig(
-      kit,
-      [
-        {
-          from: this.state.address,
-          to: this.state.CeloBet.options.address,
-          tx: txObject,
-          value: this.state.amount*10^18,
-          feeCurrency: FeeCurrency.cUSD
-        }
-      ],
-      { requestId, dappName, callback }
-    )
-    const dappkitResponse = await waitForSignedTxs(requestId)
-    const tx = dappkitResponse.rawTxs[0]
-    let result = await toTxResult(kit.web3.eth.sendSignedTransaction(tx)).waitReceipt()
-    console.log(`Hello World contract update transaction receipt: `, result)
-  }
-
 
 
   render(){
     return (
-      <View style={styles.container}>
-        <LinearGradient
-        colors={['rgba(0,0,0,0.8)', 'transparent']}
-        style={styles.background}
-      />
-        <View style={styles.top} >
-          <Button title="login()" style={styles.button}
+      <LinearGradient
+      // Button Linear Gradient
+      colors={['#4c669f', '#3b5998', '#192f6a']}
+      style={styles.button}>
+    
+      
+        <View >
+          <Button title="login()" style={styles.top}
             onPress={()=> this.login()} />
           <Text>{this.state.address}</Text>
-          <Text>cUSD Balance: {this.state.cUSDBalance}</Text>
-          <Text>Celo Balance: {this.state.celoBalance}</Text>
+          <Text style={{padding: 2, fontSize: 14}}>cUSD Balance: {this.state.cUSDBalance}</Text>
+          <Text style={{padding: 2, fontSize: 14}}>Celo Balance: {this.state.celoBalance}</Text>
         </View>
-        <Image resizeMode='contain' source={require("./assets/spin2win.png")}></Image>
-        <Text>celo bet amount </Text>
-        <TextInput
-          style={{height: 40}}
-          placeholder="Bet amount"
-          onChangeText={(text) => this.setState({text})}
-          value={this.state.text}
-        />
-        <Text style={{padding: 10, fontSize: 42}}>
-          {this.state.text}
+        <Image style={{width: '100%', height: '55%'}} source={require("./assets/spinlogo.png")}></Image>
+        <Text style={{padding: 5, fontSize: 20}}>
+          Bet amount
         </Text>
-        <Text>Low </Text>
-        <Text>Hight</Text>
+        <TextInput
+          style={styles.button}
+          onChangeText={(amount) => this.setState({amount})}
+          value={this.state.amount}
+        />
+        <Text style={{padding: 5, fontSize: 42}}>
+          {this.state.amount}
+        </Text>
+      
         <View style={styles.fixToText}>
-          <TouchableOpacity onPress={() => this.spinup()} style={styles.buttonup}>
-          <Text style={styles.buttonText}>Spin Up</Text>
+          <TouchableOpacity onPress={() => this.makeBet(1)} style={styles.buttonup}>
+          <Text style={styles.buttonText}>Spin Up  </Text>
           </TouchableOpacity>
-          <TouchableOpacity onPress={() => this.spindown()} style={styles.buttondown}>
+          <TouchableOpacity onPress={() => this.makeBet(0)} style={styles.buttondown}>
           <Text style={styles.buttonText}>Spin Down</Text>
           </TouchableOpacity>
         </View>
       
-      </View>
+      </LinearGradient>
     );
   }
 }
@@ -194,11 +135,11 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: 20,
     backgroundColor: '#673ab7',
     alignItems: 'center',
   },
   top: {
-    flex: 0.15,
     backgroundColor: 'grey',
     borderWidth: 1,
     padding: 60,
@@ -210,19 +151,23 @@ const styles = StyleSheet.create({
     fontWeight: 'bold'
   },
   button: {
-    backgroundColor: "blue",
+    padding: 2, fontSize: 20,
+    backgroundColor: "#603ab7",
+    marginTop: 20,
     padding: 20,
     borderRadius: 10,
   },
   buttonup: {
+    margin: 8,
     backgroundColor: "red",
     padding: 20,
     borderRadius: 10,
   },
   buttondown: {
     backgroundColor: "blue",
+    margin: 8,
     padding: 20,
-    borderRadius: 5,
+    borderRadius: 10,
   },
   buttonText: {
     fontSize: 20,
--- migrations/2_deploy_contracts.js
+++
@@ -1,8 +0,0 @@
-
-const NFTMap = artifacts.require("NFTMap");
-
-module.exports =async function(deployer) {
-  // deploy NFTMap contract
-  await deployer.deploy(NFTMap);
-
-};
